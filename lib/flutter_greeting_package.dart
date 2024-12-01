a048c7b47e186a3607bdcbacf1248fd4fe2575ce
Esraa Saber <esraa.saber@rology.net>
2024-12-01 19:05:47 +0200

    module

--- lib/flutter_greeting_package.dart
+++ lib/flutter_greeting_package.dart
@@ -1,7 +1,22 @@
 library flutter_greeting_package;
 
+import 'package:flutter/services.dart';
+
 /// A Greeting.
 class Greeting {
   /// Returns  HI [value].
   String sayHi(String value) => "Hi ${value}";
+
+  static const platform = MethodChannel('com.Greeting.channelname');
+
+  static Future<dynamic> someMethod(args) async {
+  try {
+  final result = await platform.invokeMethod('Greeting', args);
+  return result;
+  } on PlatformException catch (e) {
+  // Handle error
+  print("Failed to call method: '${e.message}'");
+  }
+  }
+
 }
